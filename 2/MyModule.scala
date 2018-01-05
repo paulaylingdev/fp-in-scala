400d8f4725dea9ac1fe8836d51281f554a08aee0
Paul Ayling <paulayling.dev@gmail.com>
2018-01-05 14:14:33 +0000

    merging chapter 2 together

--- 2/MyModule.scala
+++ 2/MyModule.scala
@@ -16,8 +16,18 @@ object MyModule {
     go(n, 1)
   }
 
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int = {
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    }
+    loop(n, 0, 1)
+  }
+
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
     println(factorial(3))
+    println(fib(10))
   }
 }
