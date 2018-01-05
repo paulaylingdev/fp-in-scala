0650f66b9e8ad1952eb6ebef4f6f197ee15437a2
Paul Ayling <paulayling.dev@gmail.com>
2018-01-05 14:19:43 +0000

    2.4.2

--- 2/MyModule.scala
+++ 2/MyModule.scala
@@ -1,16 +1,11 @@
 object MyModule {
-  def abs(n: Int): Int = 
+  def abs(n: Int): Int =
     if (n < 0) -n
     else n
-    
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
 
   def factorial(n: Int): Int = {
     @annotation.tailrec
-    def go(n: Int, acc: Int): Int = 
+    def go(n: Int, acc: Int): Int =
       if (n <= 0) acc
       else go(n-1, n*acc)
     go(n, 1)
@@ -25,9 +20,19 @@ object MyModule {
     loop(n, 0, 1)
   }
 
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d"
+    msg.format(n, factorial(n))
+  }
+
   def main(args: Array[String]): Unit = {
     println(formatAbs(-42))
-    println(factorial(3))
+    println(formatFactorial(7))
     println(fib(10))
   }
 }
