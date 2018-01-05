317091d89e89eaa721c1a893641cace07b5c3419
Paul Ayling <paulayling.dev@gmail.com>
2018-01-05 14:22:33 +0000

    2.4.2 formatResult

--- 2/MyModule.scala
+++ 2/MyModule.scala
@@ -20,19 +20,14 @@ object MyModule {
     loop(n, 0, 1)
   }
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
-  private def formatFactorial(n: Int) = {
-    val msg = "The factorial of %d is %d"
-    msg.format(n, factorial(n))
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d"
+    msg.format(name, n, f(n))
   }
 
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
-    println(formatFactorial(7))
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
     println(fib(10))
   }
 }
