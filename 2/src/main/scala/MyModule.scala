c2cae43fcb40c325b3ffe90bf872bcb4475ae5ab
Paul Ayling <paulayling.dev@gmail.com>
2018-01-05 16:06:27 +0000

    exercise 2.3 and 2.4

--- 2/src/main/scala/MyModule.scala
+++ 2/src/main/scala/MyModule.scala
@@ -26,12 +26,19 @@ object MyModule {
   }
 
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
     def loop(index: Int): Boolean =
       if (index >= as.length - 1) true
       else if (ordered(as(index), as(index+1))) false
       else loop(index + 1)
     loop(0)
   }
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C =
+    (a, b) => f(a)(b)
 
   def main(args: Array[String]): Unit = {
     println(formatResult("absolute value", -42, abs))
