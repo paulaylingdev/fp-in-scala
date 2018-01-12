4005a9bde89b73f3417c0b4f311c9922c76bc8db
Paul Ayling <paulayling.dev@gmail.com>
2018-01-12 14:52:15 +0000

    listing 3.2

--- src/main/scala/exercise3/List.scala
+++ src/main/scala/exercise3/List.scala
@@ -54,6 +54,18 @@ object List {
     case Cons(x, xs) => Cons(x, init(xs))
   }
 
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x,y) => x + y)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _)
+
   def main(args: Array[String]): Unit = {
     val ex1: List[Double] = Nil
     val ex2: List[Int] = Cons(1, Nil)
