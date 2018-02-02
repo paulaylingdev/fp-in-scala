1424ce2362a836ffedf6430b30591cf4da1a25ec
Paul Ayling <paulayling.dev@gmail.com>
2018-02-02 15:23:41 +0000

    Ex 4.6 orElse

--- src/main/scala/exercise4/Either.scala
+++ src/main/scala/exercise4/Either.scala
@@ -10,6 +10,11 @@ sealed trait Either[+E, +A] {
     case Left(e) => Left(e)
     case Right(a) => f(a)
   }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case Left(e) => b
+    case Right(a) => Right(a)
+  }
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
@@ -32,6 +37,12 @@ object EitherMain {
     println(valid.flatMap(a => Right(a * a)))
     println(valid.flatMap(a => Left("some error!")))
     println(error.flatMap(a => Left("some error!")))
+
+    printBlueLine("orElse")
+    println(valid.orElse(Left("nope")))
+    println(valid.orElse(Right(6)))
+    println(error.orElse(Left("nope")))
+    println(error.orElse(Right(21)))
 
   }
 }
