7b79718bf76c03e05741f39d52387d5d7b7e0c96
Paul Ayling <paulayling.dev@gmail.com>
2018-02-02 14:53:54 +0000

    Listings

--- src/main/scala/exercise4/Listing.scala
+++ src/main/scala/exercise4/Listing.scala
@@ -35,6 +35,20 @@ object Listing {
     if (xs.isEmpty) onEmpty
     else xs.sum / xs.length
 
+  def mean_2(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
+      Left("mean of empty list!")
+    else
+      Right(xs.sum / xs.length)
+
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
+    try Right(x / y)
+    catch { case e: Exception => Left(e) }
+
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch { case e: Exception => Left(e) }
+
   def main(args: Array[String]): Unit = {
 
     def printBlueln(a: Any) =
