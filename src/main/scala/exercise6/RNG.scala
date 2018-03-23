6f52e2284104ab3f758780f79580167a1305abeb
Paul Ayling <paulayling.dev@gmail.com>
2018-03-23 17:06:48 +0000

    Ex 6.3 intDouble, doubleInt, double3

--- src/main/scala/exercise6/RNG.scala
+++ src/main/scala/exercise6/RNG.scala
@@ -18,12 +18,38 @@ object Main extends App {
     val (result, newRng) = rng.nextInt
     (if (result < 0) -(result + 1) else result, newRng)
   }
+
   def double(rng: RNG): (Double, RNG) = {
-   val (result, newRng) = nonNegativeInt(rng)
+    val (result, newRng) = nonNegativeInt(rng)
     (result.toDouble / Int.MaxValue, newRng)
   }
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (integer, newRng) = nonNegativeInt(rng)
+    val (doubleFloat, newerRng) = double(newRng)
+    ((integer, doubleFloat), newerRng)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val ((integer, double), newRng) = intDouble(rng)
+    ((double, integer), newRng)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d1, rng1) = double(rng)
+    val (d2, rng2) = double(rng1)
+    val (d3, finalRng) = double(rng2)
+    ((d1, d2, d3), finalRng)
+  }
+
 
   println(nonNegativeInt(SimpleRNG(5)))
 
   println(double(SimpleRNG(100)))
+
+  println(intDouble(SimpleRNG(5)))
+
+  println(doubleInt(SimpleRNG(5)))
+
+  println(double3(SimpleRNG(5)))
 }
