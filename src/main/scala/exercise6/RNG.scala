84faa29e72dd97f4829f090cd55a1d0f802e9e0f
Paul Ayling <paulayling.dev@gmail.com>
2018-04-06 16:17:15 +0100

    Ex 6 get set modify

--- src/main/scala/exercise6/RNG.scala
+++ src/main/scala/exercise6/RNG.scala
@@ -42,6 +42,15 @@ object State {
     (state: S) => (a, state)
   }
 
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get
+    _ <- set(f(s))
+  } yield ()
+
   def sequence[A, S](fs: List[State[S, A]]): State[S, List[A]] =
     fs.reverse.foldLeft(unit[List[A], S](List.empty[A]))(
       (list: State[S, List[A]], item: State[S, A]) => {
