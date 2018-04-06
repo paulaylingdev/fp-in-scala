20cfbd7ca4eef957a5911c4a67a1c7d9e2ccb196
Paul Ayling <paulayling.dev@gmail.com>
2018-04-06 16:46:34 +0100

    Ex 6.11 Candy

--- src/main/scala/exercise6/RNG.scala
+++ src/main/scala/exercise6/RNG.scala
@@ -57,8 +57,8 @@ object State {
         item.map2(list)(_ :: _)
       }
     )
-
 }
+
 
 object Main extends App {
   type Rand[+A] = RNG => (A, RNG)
@@ -180,4 +180,32 @@ object Main extends App {
   println(seqInts(10)(SimpleRNG(1)))
 
   println(nonNegativeLessThan(5)(SimpleRNG(2)))
+}
+
+sealed trait Input
+
+case object Coin extends Input
+
+case object Turn extends Input
+
+case class Machine(locked: Boolean, candies: Int, coins: Int)
+
+object FSA extends App {
+  def update = (i: Input) => (s: Machine) =>
+    (i, s) match {
+      case (_, Machine(_, 0, _)) => s
+      case (Coin, Machine(false, _, _)) => s
+      case (Turn, Machine(true, _, _)) => s
+      case (Coin, Machine(true, candy, coin)) =>
+        Machine(locked = false, candy, coin + 1)
+      case (Turn, Machine(false, candy, coin)) =>
+        Machine(locked = true, candy - 1, coin)
+    }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- State.sequence(inputs map (State.modify[Machine] _ compose update))
+    s <- State.get
+  } yield(s.coins, s.candies)
+
+  println(simulateMachine(List(Coin, Turn)).run(Machine(locked = true, 10, 4)))
 }
