20122487f3dd5f8a4015d582914cea340dd00fd1
Paul Ayling <paulayling.dev@gmail.com>
2018-04-13 16:45:54 +0100

    7.5 Listing

--- src/main/scala/exercise7/Par.scala
+++ src/main/scala/exercise7/Par.scala
@@ -1,9 +1,41 @@
 package main.scala.exercise7
 
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
+
 /*
 Exercise 7.1
 map2 signature
- */
+
 trait Par[A] {
   def map2[B, C](a: => Par[A], b: => Par[B])(f: (A, B) => C): Par[C]
+}
+*/
+
+object Par {
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+
+    def get(timeout: Long, units: TimeUnit): A = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call: A = a(es).get
+    })
+
 }
