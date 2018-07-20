90f821fe42f3a72ddd29f52d175fad08ee827b5c
Paul Ayling <paulayling.dev@gmail.com>
2018-07-20 17:26:13 +0100

    Ex 8.2 max

--- src/test/scala/excercise8/PropTest.scala
+++ src/test/scala/excercise8/PropTest.scala
@@ -1,15 +1,23 @@
 package excercise8
 
+import org.scalacheck.Gen._
 import org.scalacheck.{Gen, Prop}
 import org.scalacheck.Prop.forAll
-import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.Checkers._
+import org.scalatest.{FlatSpec, Matchers}
 
 class PropTest extends FlatSpec with Matchers {
 
     "sum" should "sum up a list of integers" in {
-      val intList = Gen.listOf(Gen.choose(0, 100))
+      val intList = listOf(choose(0, 100))
       val prop: Prop = forAll(intList)(ns => ns.reverse.sum == ns.sum)
+      check(prop)
+    }
+
+    "max" should "find the maximum integer in a list" in {
+      val max = 125
+      val intList = listOf(choose(0, max)) suchThat(_.contains(max))
+      val prop = forAll(intList)(ns => ns.max == max)
       check(prop)
     }
 }
