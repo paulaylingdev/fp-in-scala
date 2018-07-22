6533909d619e23a8aed3d8a760a8d695b486c78c
Paul Ayling <paulayling.dev@gmail.com>
2018-07-22 12:50:49 +0100

    Ex 8.4

--- src/main/scala/exercise8/Prop.scala
+++ src/main/scala/exercise8/Prop.scala
@@ -1,6 +1,6 @@
 package exercise8
 
-import main.scala.exercise6.{RNG, State}
+import main.scala.exercise6.{Main, RNG, SimpleRNG, State}
 
 trait Prop {
   def check: Boolean
@@ -19,6 +19,9 @@ case class Gen[A](sample: State[RNG, A])
 
 object Gen {
   def choose(start: Int, stopExclusive: Int): Gen[Int] = {
-    ???
+    Gen(State(Main.nonNegativeInt).map {
+      generatedInt: Int => generatedInt + start % (stopExclusive - start)
+    })
+
   }
 }
