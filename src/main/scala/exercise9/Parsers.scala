b2868a88b195cb8ccfe112197483ab711c57214c
Paul Ayling <paulayling.dev@gmail.com>
2018-10-26 13:16:55 +0100

    Ex 9.6

--- src/main/scala/exercise9/Parsers.scala
+++ src/main/scala/exercise9/Parsers.scala
@@ -3,15 +3,18 @@ package exercise9
 import exercise8.Prop._
 import exercise8.{Gen, Prop}
 
+import scala.util.matching.Regex
+
 trait Parsers[ParseError, Parser[+ _]] {
   self =>
+
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
 
   implicit def string(s: String): Parser[String]
 
-  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  def or[A](s1: Parser[A], s2: => Parser[A]): Parser[A]
 
   implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
 
@@ -31,9 +34,13 @@ trait Parsers[ParseError, Parser[+ _]] {
 
   def many1[A](p: Parser[A]): Parser[List[A]] = map2(p, many(p))(_ :: _)
 
-  def product[A, B](p: Parser[A], p2: Parser[B]): Parser[(A, B)]
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)]
 
-  def map2[A, B, C](p: Parser[A], p2: Parser[B])(f: (A, B) => C): Parser[C] = product(p, p2).map(prod => f(prod))
+  def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] = product(p, p2).map(prod => f(prod))
+
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  implicit def regex(r: Regex): Parser[String]
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
@@ -51,6 +58,8 @@ trait Parsers[ParseError, Parser[+ _]] {
     def product[B](p2: Parser[B]): Parser[(A, B)] = self.product(p, p2)
 
     def map2[B, C](p2: Parser[B])(f: (A, B) => C): Parser[C] = self.map2(p, p2)(f)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] = self.flatMap(p)(f)
   }
 
   object Laws {
